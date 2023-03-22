b674d3717ce30cd59c6468a6164a5e6ed5dd805d
oksuzd <oksuzdal@mail.ru>
2023-03-22 09:33:19 -0300

    some mark-up and styles changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { NotFoundComponent } from "@core/pages/not-found/not-found.component";
 const routes: Routes = [
   {path: '', redirectTo: 'main-page', pathMatch: 'full'},
   {path: 'main-page', component: MainPageComponent},
+  {path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'youtube',
+    loadChildren: () => import('./youtube/youtube.module').then(m => m.YoutubeModule)},
   {path: '**', component: NotFoundComponent},
 ];
 
